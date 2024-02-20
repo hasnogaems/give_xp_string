efb21e2d59ba0282b46027ade117c00d8c43eef0
myPC <user@DESKTOP-VE36L8M>
2024-02-21 01:08:24 +0300

    make3 works with both tetsts

--- src/main.c
+++ src/main.c
@@ -1,18 +1,6 @@
 #include "s21_sscanf.h"
 #include "s21_tests.h"
-void run_testcase(Suite *testcase) {
-  static int counter_testcase = 1;
-  if (counter_testcase > 1) putchar('\n');
-  printf("CURRENT TEST: %d\n", counter_testcase);
-  counter_testcase++;
 
-  SRunner *sr = srunner_create(testcase);
-
-  srunner_set_fork_status(sr, CK_NOFORK);
-  srunner_run_all(sr, CK_NORMAL);
-
-  srunner_free(sr);
-}
 
 
 void run_tests(void) {
@@ -23,7 +11,19 @@ void run_tests(void) {
     run_testcase(*current_testcase);
   }
 }
+void run_testcase(Suite *testcase) {
+  static int counter_testcase = 1;
+  if (counter_testcase > 1) putchar('\n');
+  printf("CURRENT TEST: %d\n", counter_testcase);
+  counter_testcase++;
+SRunner *sr=calloc(counter_testcase, sizeof(SRunner *));
+   sr = srunner_create(testcase);
 
+  //srunner_set_fork_status(sr, CK_NOFORK);
+  srunner_run_all(sr, CK_NORMAL);
+
+  srunner_free(sr);
+}
 int main(void) {
   run_tests();
   return 0;
---
+++ src/main2.c
@@ -0,0 +1,18 @@
+#include "s21_tests.h"
+#include <check.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void) {
+  Suite *suite = s21_string_suite();
+  SRunner *suite_runner = srunner_create(suite);
+  srunner_run_all(suite_runner, CK_VERBOSE);
+  int failed_count = srunner_ntests_failed(suite_runner);
+  srunner_free(suite_runner);
+  suite = suite_sscanf();
+  suite_runner = srunner_create(suite);
+  srunner_run_all(suite_runner, CK_VERBOSE);
+  failed_count += srunner_ntests_failed(suite_runner);
+  return (failed_count == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
